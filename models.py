22ae45df6cda747db5c861188f517d342fba0e78
Manh Thang <manhthang110207@gmail.com>
2026-06-04 15:38:58 +0700

    Them 2 db quan ly nguoi dung va chuong truyen

---
+++ models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+db = SQLAlchemy()
+
+class Truyen(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ten_truyen = db.Column(db.String(100), nullable = False)
+    tac_gia = db.Column(db.String(100), nullable = False)
+    the_loai = db.Column(db.String(100), nullable = False)
+    mo_ta = db.Column(db.Text, nullable = False)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.text, nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    role = db.Column(db.String(20), nullable=False, default='user')
+
+class Chuong(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ten_chuong = db.Column(db.String(100), nullable=False)
+    noi_dung = db.Column(db.Text, nullable=False)
+    truyen_id = db.Column(db.Integer, db.ForeignKey('Truyen.id'), nullable=False)
